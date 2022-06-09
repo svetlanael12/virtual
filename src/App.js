d1a3d6327227a4b6f90f10d4fbae9da88300710c
Svetlana <svetlana_el12@mail.ru>
2022-06-09 17:29:04 +0500

    block-service, up-button, burger-menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import './App.css';
 import Title from './components/mainBlock/Title';
 import Header from './components/navigation/Header';
+import ServiceBlock from './components/mainBlock/ServiceBlock';
+import { useState, useEffect } from 'react';
 
 
 function App() {
+  const [scroll, setScroll] = useState(0);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
+  const handleUpButton = () => {
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  
+
+
   return (
-    <div className="App">
+    <div className="App" >
       <header>
-        <Header />
+        <Header nav={scroll} />
       </header>
       <main>
         <Title />
+        <ServiceBlock />
       </main>
+
+      <button
+            className={scroll > 150 ? "button-up" : "none"}
+            onClick={handleUpButton}
+        >
+            ↑
+        </button>
     </div>
+    
   );
 }
 
